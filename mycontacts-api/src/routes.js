60e801eb444bb233ff4de43527b2dffc1abc1c67
Marcos Henrique <marcoshenriqueh393@gmail.com>
2021-09-27 18:14:22 -0300

    feat: estudando sobre middlewares

--- mycontacts-api/src/routes.js
+++ mycontacts-api/src/routes.js
@@ -4,7 +4,14 @@ const ContactController = require('./app/controllers/ContactController');
 
 const router = Router();
 
-router.get('/contacts', ContactController.index);
+router.get(
+  '/contacts',
+  (request, response, next) => {
+    request.appId = 'MeuAppId';
+    next();
+  },
+  ContactController.index,
+);
 router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 

09ba7969d30b50967eb762c3be3027b1b949c397
Marcos Henrique <marcoshenriqueh393@gmail.com>
2021-09-28 19:54:55 -0300

    feat: criando tabela de contatos

--- mycontacts-api/src/database/schema.sql
+++ mycontacts-api/src/database/schema.sql
@@ -6,3 +6,12 @@ Create TABLE IF NOT EXISTS categories (
   id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
   name VARCHAR NOT NULL
 ); --> criando tabela de categorias caso não exista
+
+Create TABLE IF NOT EXISTS contacts (
+  id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+  name VARCHAR NOT NULL,
+  email VARCHAR UNIQUE,
+  phone VARCHAR,
+  category_id UUID,
+  FOREIGN KEY(category_id) REFERENCES categories(id) --> foreign key de categoria
+); --> criando tabela de contatos caso não exista

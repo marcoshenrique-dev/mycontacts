af88916626928428f381173549867235f390b615
Marcos Henrique <marcoshenriqueh393@gmail.com>
2021-09-28 07:11:46 -0300

    feat: criando rota de edição de contatos

--- mycontacts-api/src/routes.js
+++ mycontacts-api/src/routes.js
@@ -4,15 +4,9 @@ const ContactController = require('./app/controllers/ContactController');
 
 const router = Router();
 
-router.get(
-  '/contacts',
-  (request, response, next) => {
-    request.appId = 'MeuAppId';
-    next();
-  },
-  ContactController.index,
-);
+router.get('/contacts', ContactController.index);
 router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
+router.post('/contacts', ContactController.store);
 
 module.exports = router;

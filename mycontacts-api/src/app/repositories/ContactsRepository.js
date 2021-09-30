66b0af7216d1955c996bfdef9f0ae66e5bce8eef
Marcos Henrique <marcoshenriqueh393@gmail.com>
2021-09-30 10:04:08 -0300

    feat: refatorando o método update

--- mycontacts-api/src/app/repositories/ContactsRepository.js
+++ mycontacts-api/src/app/repositories/ContactsRepository.js
@@ -58,22 +58,15 @@ class ContactRepository {
     return row;
   }
 
-  update(id, {
+  async update(id, {
     name, email, phone, category_id,
   }) {
-    return new Promise((resolve) => {
-      const updatedContact = {
-        id,
-        name,
-        email,
-        phone,
-        category_id,
-      };
-      contacts = contacts.map((contact) => (
-        contact.id === id ? updatedContact : contact
-      ));
-      resolve(updatedContact);
-    });
+    const [row] = await db.query(`
+      UPDATE contacts
+      SET name = $1, email = $2, phone = $3, category_id = $4
+      WHERE id = $5
+    `, [name, email, phone, category_id, id]);
+    return row;
   }
 }
 
